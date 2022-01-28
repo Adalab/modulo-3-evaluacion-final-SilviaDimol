6328caccea21950f9c7ad2e6001989186e1333e7
Silvia Dimol <silviadmolina@gmail.com>
2022-01-28 10:12:50 +0100

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Route, Switch, useRouteMatch, NavLink } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import "../styles/App.css";
 import Filters from "./Filters";
 import CharacterList from "./CharacterList";
